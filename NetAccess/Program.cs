0f7c5342cf6b3d567431ecf6c9ae4a887edb28da
Karthik Nishanth <nishanthkarthik@live.com>
2015-10-16 16:11:19 +0530

    First build

--- NetAccess/Program.cs
+++ NetAccess/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace NetAccess
 {
@@ -6,7 +7,41 @@ namespace NetAccess
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
+			LibNetAccess net = new LibNetAccess ();
+			Console.Write ("Roll No. : ");
+			string roll = Console.ReadLine ();
+			Console.Write ("Password : ");
+			string pass = GetStringPassword();
+			net.AddCredentials (roll, pass);
+			if (net.Authenticate ())
+				Console.WriteLine ("\nUsage : " + net.DataUsage);
+			else
+				Console.WriteLine ("Login error");
+		}
+
+		public static string GetStringPassword ()
+		{
+			StringBuilder _pwd = new StringBuilder ();
+
+			while (true)
+			{
+				ConsoleKeyInfo i = Console.ReadKey (true);
+				if (i.Key == ConsoleKey.Enter)
+				{
+					break;
+				}
+				if (i.Key != ConsoleKey.Backspace)
+				{
+					_pwd.Append (i.KeyChar);
+					Console.Write ("*");
+				} else if (_pwd.Length > 0)
+				{
+					_pwd.Remove (_pwd.Length - 1, 1);
+					Console.Write ("\b \b");
+				}
+			}
+			return _pwd.ToString ();
+
 		}
 	}
 }
